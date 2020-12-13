d501f1263bf42dc78f6477dab45d04a4e93fc929
pradeep kumar yadav <60665024+pradeepkumarjse@users.noreply.github.com>
2020-12-13 13:13:07 +0530

    Add files via upload

---
+++ FirstJDBC.java
@@ -0,0 +1,39 @@
+import java.sql.*;
+class FirstJDBC
+{
+	public static void main(String args[]) 
+	{
+		
+		//loading driver
+		try{
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		String url="jdbc:mysql://localhost:3306/youtube1";
+		String username="root";
+		String password="Pradeep@1234";
+		Connection con=DriverManager.getConnection(url,username,password);
+		
+		
+		
+		if(con.isClosed())
+		{
+			System.out.println("Connection is closed...");
+		}
+		else
+		{
+			System.out.println("Connection created sucessfully....");
+		}
+		
+		
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		
+		
+	
+	}
+}
