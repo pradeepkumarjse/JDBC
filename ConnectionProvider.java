b7310484389f73fc11616cc88a3cf23f003e970b
pradeep kumar yadav <60665024+pradeepkumarjse@users.noreply.github.com>
2020-12-14 09:27:57 +0530

    Create ConnectionProvider.java

---
+++ ConnectionProvider.java
@@ -0,0 +1,31 @@
+
+package com.jdbc.practice;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+class ConnectionProvider {
+    private static Connection con;
+    
+    public static Connection getConnection(){
+        try{
+            if(con==null) {
+                
+                
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                
+                con=DriverManager.getConnection("jdbc:mysql://localhost:3306/youtube1","root","Pradeep@1234");
+                
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        
+            return con;
+
+    }
+    
+    
+    
+}
